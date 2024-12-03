aed93d087058f82d95b2014ac1de8924b93e0a45
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2024-12-03 14:33:52 +0600

    fake data

---
+++ knowledge-cafe/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1 className='text-6xl bg-orange-800'>Vite + React</h1>
+    </div>
+  )
+}
+
+export default Header
