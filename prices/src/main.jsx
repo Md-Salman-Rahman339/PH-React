c5171f526f003726d6ca93075c73eb30db22654a
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2024-12-07 04:46:30 +0600

    router

---
+++ prices/src/main.jsx
@@ -0,0 +1,24 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+  {
+    path: "/about",
+    element: <div>Hello from Router</div>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
