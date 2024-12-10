6cc1a6307f37a0b199f1c88ef8a3811c3d27b4eb
Md-Salman-Rahman339 <salmanrahman339@gmail.com>
2024-12-11 02:49:01 +0600

    navbar and footer and all setup

---
+++ Book-Vibe/src/main.jsx
@@ -0,0 +1,39 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import { Root } from 'postcss';
+import Roots from './components/Roots/Roots';
+import Errorpage from './components/Errorpage/Errorpage';
+import Home from './components/Home/Home';
+import Dashboard from './components/Dashboard/Dashboard';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Roots></Roots>,
+    errorElement:<Errorpage></Errorpage>,
+    children:[
+     {
+      path:'/home',
+      element:<Home></Home>,
+     },
+     {
+      path:'dashboard',
+      element:<Dashboard></Dashboard>
+     },
+   
+    ]
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
